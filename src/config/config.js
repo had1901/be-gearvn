5dd74163175ffb5afa68103d9d24f1df26119778
had191 <senna2k20@gmail.com>
2025-09-04 11:07:46 +0700

    Remove component, fix css

---
+++ src/config/config.js
@@ -0,0 +1,34 @@
+// require('dotenv').config()
+
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DATABASE_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "mysql",
+//     logging: false,
+//     define: { freezeTableName: true }
+//   },
+//   test: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "mysql",
+//     logging: false,
+//     define: { freezeTableName: true }
+//   },
+//   production: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "mysql",
+//     logging: false,
+//     define: { freezeTableName: true }
+//   }
+// }
